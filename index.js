b18c4163929a68c366400a161e518669a19caba1
Andres Soibelzon <a.soibelzon@gmail.com>
2023-01-03 19:05:35 +0000

    correccion

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ let numberCard = document.getElementById("numberCard");
 
 window.onload = function() {
     let img = ["♦", "♥" ,"♠" ,'♣']; // between 1 to 4
-    let imgCard = img[(Math.floor(Math.random() * 4))];
+    let imgCard = img[(Math.floor(Math.random() * img.length))];
     imgUp.innerHTML = imgCard;
     imgDown.innerHTML = imgCard;
 
     let nmb = ["A", "2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8", "9", "J", "Q", "K"]; // between 1 to 12
-    let nmbCard = nmb[(Math.floor(Math.random() * 13))];
-    numberCard.innerHTML = nmbCard
+    let nmbCard = nmb[(Math.floor(Math.random() * nmb.length))];
+    numberCard.innerHTML = nmbCard;
 
+    if(imgCard === "♦" || imgCard === "♥"){
+        imgUp.style.color = "red"
+        imgDown.style.color = "red"
+        numberCard.style.color = "red"
+    }
 }
 
